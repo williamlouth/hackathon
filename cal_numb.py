29ed6fe97818c2ef6882d17f761def5bd25510b6
william <williamlouth@outlook.com>
2019-03-15 17:22:53 +0000

    fix cal_numb

--- cal_numb.py
+++ cal_numb.py
@@ -10,23 +10,32 @@ cur = conn.cursor()
 #print(cur.fetchall())
 a = pd.read_sql("select storm_stint.student_id,type,grade from storm_stint inner join storm_review on storm_stint.id = storm_review.stint_id;",conn)
 #print(a)
+
+cur.execute("update storm_student set bar_number=0;" )
 for i in range(a.shape[0]):
     b = a.loc[i].values
     if b[1] == "Bar Back":
         print("HIT")
-        #cur.execute("update storm_student set bar_number=100;" )
+        print(b[0])
+        cur.execute("select bar_number from storm_student where baseuser_ptr_id=%s;",[int(b[0])])
+        #cur.execute("select bar_number from storm_student where baseuser_ptr_id =%s;" , int(b[0]))
+        c = cur.fetchall()
+        d = int(c[0][0])
+        d+=1
+        cur.execute("update storm_student set bar_number=%s where baseuser_ptr_id=%s;" , (d,int(b[0])) )
+        #cur.execute("update storm_student set bar_number=%s where baseuser_ptr_id = %s;" %(b[1],b[0]))
 
 
     #print(b)
 
-cur.execute("update storm_student set bar_number=100;" )
+#cur.execute("update storm_student set bar_number=100;" )
 cur.execute("commit;")
 
 cur.execute("select column_name from information_schema.columns where table_name = 'storm_student';")
 print(cur.fetchall())
 #print(cur.fetchall())
 
-cur.execute("select bar_number from storm_student where bar_number = 100;")
+cur.execute("select bar_number,baseuser_ptr_id from storm_student where bar_number != 0;")
 print(cur.fetchall())
 
 
