c72e6d0c68dfdb34baae3668ffae1d61fdee8770
william <williamlouth@outlook.com>
2019-03-15 19:54:18 +0000

    working?

--- cal_numb.py
+++ cal_numb.py
@@ -25,36 +25,36 @@ a = pd.read_sql("select storm_stint.student_id,type,grade from storm_stint inner
 cur.execute("update storm_student set bar_number=0;" )
 nump = c.values
 for j in range(18):
+    key_num_test = keywords_vals[j] +'_number'
+    key_total_test = keywords_vals[j] +'_total'
+    key_average_test = keywords_vals[j] +'_average'
+
+    cur.execute(sql.SQL("update storm_student set {}=0;").format(sql.Identifier(str(key_num_test[0]))))
+    cur.execute(sql.SQL("update storm_student set {}=0;").format(sql.Identifier(str(key_total_test[0]))))
+    cur.execute(sql.SQL("update storm_student set {}=0;").format(sql.Identifier(str(key_average_test[0]))))
+    cur.execute("commit;")
+
     print(keywords_vals[j])
     for i in range(18):
         if nump[j][i] != "nan" and isinstance(nump[j][i],str) :
             to_test = nump[j][i]
-            print(to_test)
-            key_num_test = keywords_vals[j] +'_number'
             num_test = to_test+'_number'
             total_test = to_test+'_total'
             average_test = to_test+'_average'
             for i in range(a.shape[0]):
                 b = a.loc[i].values
-                print(b)
                 if b[1] == to_test:
-                    print(key_num_test[0])
-                    print(b[0])
                     #cur.execute("select %s from storm_student where baseuser_ptr_id=%s;",(str(key_num_test[0]),int(b[0])))
                     cur.execute(sql.SQL("select {} from storm_student where baseuser_ptr_id=%s;").format(sql.Identifier(key_num_test[0])),[int(b[0])])
                     #query = "select %s from storm_student where baseuser_ptr_id=%s;"
                     #cur.execute(query,([key_num_test[0]],int(b[0])))
                     #cur.execute("select bar_number from storm_student where baseuser_ptr_id =%s;" , int(b[0]))     c = cur.fetchall()
                     c = cur.fetchall()
-                    print("hu")
-                    print(c)
                     d = int(c[0][0])
-                    print("d")
-                    print(d)
                     d+=1
-                    cur.execute("update storm_student set %s=%s where baseuser_ptr_id=%s;" , (num_test,d,int(b[0])) )
+                    #cur.execute("update storm_student set %s=%s where baseuser_ptr_id=%s;" , (num_test,d,int(b[0])) )
 
-                    cur.execute(sql.SQL("update storm_student set %s=%s where baseuser_ptr_id=%s;").format(sql.Identifier(key_num_test[0])),[int(b[0])])
+                    cur.execute(sql.SQL("update storm_student set {}=%s where baseuser_ptr_id=%s;").format(sql.Identifier(key_num_test[0])),[d,int(b[0])])
                     
 
 #for i in range(a.shape[0]):
