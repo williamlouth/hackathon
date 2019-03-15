f6d6add127fd1ae4f5e0f6b0d1963e806611535f
william <williamlouth@outlook.com>
2019-03-15 15:14:42 +0000

    add column adding script

---
+++ add_cols.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import psycopg2 as py
+import login
+
+conn = login.conn
+
+cur = conn.cursor()
+cur.execute("alter table storm_student add bar float;")
+cur.execute("select * from storm_student LIMIT 1;")
+print(cur.fetchall())
+cur.execute("commit;")
+cur.execute("select column_name from information_schema.columns where table_name = 'storm_student';")
+print(cur.fetchall())
+#print(cur.fetchall())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
