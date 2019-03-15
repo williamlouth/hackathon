bef38cfb61b14360e9876118c3745d95347e0d58
william <williamlouth@outlook.com>
2019-03-15 13:12:05 +0000

    hi

--- test.py
+++ test.py
@@ -4,7 +4,8 @@ import login
 conn = login.conn
 #conn  = py.connect("dbname = hack user = will")
 cur = conn.cursor()
-print(cur.execute("select * FROM storm_stint LIMIT 1;"))
+print(cur.execute("select column_name from information_schema.columns where table_name = 'storm_business';"))
+#print(cur.execute("dt;"))
 print(cur.fetchall())
 
 
