f04f8e2b09bfc0cd8a8d219c60dc4b8460fb264a
Gustavs Zilgalvis <gzilgalvis@gmail.com>
2019-03-15 17:22:09 +0000

    Update average_scores.py

---
+++ average_scores.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import psycopg2 as py
+
+conn = py.connect("dbname=hackathon user=postgres password=pw")
+cur = conn.cursor()
+cur.execute("SELECT student_id, grade FROM storm_review;")
+a = cur.fetchall()
+
+d = {}
+
+for k, v in a:
+	d.setdefault(k, [k]).append(v)
+b = map(tuple, d.values())
+
+rvw_tuple = []
+
+for j in b:
+	rvw_tuple.append(j)
+
+reviews = []
+
+for i in rvw_tuple:
+
+	reviews.append(list(i))
+
+print(reviews)
+
+
+
