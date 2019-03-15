4c6eaebd4e7fe50b5b514dcf8651d7a312da345d
CezaryKlimczuk <cezary.klimczuk.17@ucl.ac.uk>
2019-03-15 13:18:21 +0000

    sth

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 import psycopg2 as py
 
 
-conn  = py.connect("dbname = hack user = will")
+conn = py.connect("dbname = hackathon user = postgres password=Koteczek123")
 cur = conn.cursor()
-print(cur.execute("select * FROM storm_stint;"))
+print(cur.execute("SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = N'YourTableName'"))
 print(cur.fetchall())
+
 
 
