6df25ab17656a899612d9922a70d5993645476c1
Gustavs Zilgalvis <gzilgalvis@gmail.com>
2019-03-15 13:15:54 +0000

    Update test.py

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import login
 
 conn = login.conn
 #conn  = py.connect("dbname = hack user = will")
+
 cur = conn.cursor()
 print(cur.execute("select * FROM storm_stint LIMIT 1;"))
 print(cur.fetchall())
