ae6eeb93687189cfadc450b756c4abb54d0697b4
william <williamlouth@outlook.com>
2019-03-15 20:17:30 +0000

    hi

--- cal_numb.py
+++ cal_numb.py
@@ -3,9 +3,9 @@ from psycopg2 import sql
 import math
 import pandas as pd
 import psycopg2 as py
+import login
 
-conn = py.connect("dbname=hackathon user=postgres password=pw")
-
+conn =  login.conn
 cur = conn.cursor()
 #cur.execute("select storm_stint.student_id,type,grade from storm_stint inner join storm_review on storm_stint.id = storm_review.stint_id;")
 #cur.execute("select * from storm_student LIMIT 1;")
