174ee958f33d19f4bb7de2a47ee854f47da66164
william <williamlouth@outlook.com>
2019-03-15 12:30:18 +0000

    hi

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 import psycopg2 as py
+import login
 
-
-conn  = py.connect("dbname = hack user = will")
+conn = login.conn
+#conn  = py.connect("dbname = hack user = will")
 cur = conn.cursor()
-print(cur.execute("select * FROM storm_stint;"))
+print(cur.execute("select * FROM storm_stint LIMIT 1;"))
 print(cur.fetchall())
 
 
