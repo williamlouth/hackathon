5b7ed7e7f0fac6416d0e498ed170b2ec99ad7152
Gustavs Zilgalvis <gzilgalvis@gmail.com>
2019-03-16 16:32:21 +0000

    time

--- time.py
+++ time.py
@@ -3,16 +3,31 @@ import psycopg2 as py
 import login
 
 conn = login.conn
-#conn  = py.connect("dbname = hack user = will")
-
 cur = conn.cursor()
-#print(cur.execute("select * FROM storm_stint LIMIT 1;"))
-#print(cur.fetchall())
-print(cur.execute("select column_name from information_schema.columns where table_name = 'storm_review';"))
-#print(cur.execute("dt;"))
+
+# Getting stint times
+
+cur.execute("select * from storm_stint LIMIT 1;")
+stints = cur.fetchall()
+
+stinttimes = []
+
+for stint in stints:
+    ii = [stint[0]] + [stint[3]] + [stint[5]]
+    stinttimes.append(ii)
+
+print(stinttimes[0])
+
+# Getting times when students available
+
+cur.execute("select * from storm_studentavailability LIMIT 1;")
+studentav = cur.fetchall()
+print(studentav)
+
+cur.execute("select column_name from information_schema.columns where table_name = 'storm_studentavailability';")
 print(cur.fetchall())
-#cur.execute("select * from storm_student LIMIT 1;")
-#print(cur.fetchall())
+
+# --
 
 #print(cur.execute("select column_name from information_schema.columns where table_name = 'storm_business';"))
 #print(cur.execute("copy storm_stint to 'stint.csv' delimeter ',' csv header;"))
@@ -22,8 +37,8 @@ print(cur.fetchall())
 #a.to_pickle('student.txt')
 #print(a)
 
-cur.execute("select * from storm_student LIMIT 100;")
-print(cur.fetchall())
+#cur.execute("select * from storm_student LIMIT 100;")
+#print(cur.fetchall())
 
 
 # Instead of manually creating categories, it may be worth seeing whether there are correlations 
