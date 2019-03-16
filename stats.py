22fc0b1842c5ba5b6beb43f337ab1574867804e5
CezaryKlimczuk <cezary.klimczuk.17@ucl.ac.uk>
2019-03-16 18:53:40 +0000

    koko

--- stats.py
+++ stats.py
@@ -1,7 +1,8 @@
 import numpy as np
-dist = np.array([2440, 603, 128, 54, 17])
+
 
 def normalize(distribution, grade):
     return np.sum(distribution[5 - grade:]) / np.sum(distribution)
+
 
 # Investment Banking is for losers
