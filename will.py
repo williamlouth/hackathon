90d19768bd299f54b3a8bc8393cfd6221f551173
CezaryKlimczuk <cezary.klimczuk.17@ucl.ac.uk>
2019-03-16 13:20:59 +0000

    algo

--- will.py
+++ will.py
@@ -1,7 +1,9 @@
 import numpy as np
 import time
 
-M = np.random.rand(1200, 20)
+
+M = np.random.rand(140, 9)
+limit = min(len(M[0]), len(M[1]))
 #print(M)
 
 def delete_smallest(A):             # deletes the smallest entry in the matrix
@@ -20,18 +22,35 @@ def one_entry_column(A):            # checks if there is a column with one stude
         if count_not_nans(A[:,col]) == 1:
             return col
 
+def one_entry_row(A):               # checks if there is a student with only one job left
+    for row in range(len(A)):
+        if count_not_nans(A[row]) == 1:
+            return row
+
+def options_left(A):
+    A = A[~np.isnan(A).all(axis=1)]
+    A = A[:, ~np.isnan(A).all(axis=0)]
+    return A.shape
+
+
 pairs = []  # the list of matches
 
 def delete_pairs(A):    # turns student row into nans if assigned to a job
     while True:
         col = one_entry_column(A)   # checks if there is a column with one student left
+        row = one_entry_row(A)
         if col is not None:    # checking if there is a column with only 1 student available
             row = np.where(A[:,col] == np.nanmax(A[:,col]))[0][0]   # finding the last available student
-            print([row, col])
+            #print([row, col])
             pairs.append([row, col])        # appending the match to the list
             A[row] *= np.nan    # setting student to unavailable for other jobs
+        elif options_left(A)[0] == options_left(A)[1] and row is not None:
+            col = np.where(A[row] == np.nanmax(A[row]))[0][0]
+            pairs.append([row, col])
+            A[:,col] *= np.nan
         else:
             return A
+
 
 def iterate(A):     # implementing the process
     return delete_pairs(delete_smallest(A))
@@ -39,10 +58,11 @@ def iterate(A):     # implementing the process
 st = time.time()
 while True:
     M = iterate(M)
-    #print(M,'\n\n')
-    if len(pairs) == 17:
+    print(M,'\n\n')
+    if len(pairs) == limit:
         break
 print(pairs)
+print(len(pairs))
 end = time.time()
 print(end - st)
 
