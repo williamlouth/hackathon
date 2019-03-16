3e8d94cd2b15e7796d5bb63dc078d2086c1dc025
william <williamlouth@outlook.com>
2019-03-16 10:18:38 +0000

    added bus_numb.py

--- add_cols.py
+++ add_cols.py
@@ -15,13 +15,28 @@ for i in b:
     tot = i[0] + '_total'
     number = i[0] + '_number'
     average = i[0] + '_average'
-    cur.execute("alter table storm_student add %s float;" % tot)
-    cur.execute("alter table storm_student add %s int;" % number)
-    cur.execute("alter table storm_student add %s float;" % average)
+    #cur.execute("alter table storm_student add %s float;" % tot)
+    #cur.execute("alter table storm_student add %s int;" % number)
+    #cur.execute("alter table storm_student add %s float;" % average)
     #cur.execute("alter table storm_student drop %s ;" % tot)
     #cur.execute("alter table storm_student drop %s ;" % average)
     #cur.execute("alter table storm_student drop %s ;" % number)
 
+
+#cur.execute("alter table storm_business add past_1 int;" )
+#cur.execute("alter table storm_business add past_2 int;" )
+#cur.execute("alter table storm_business add past_3 int;" )
+#cur.execute("alter table storm_business add past_4 int;" )
+#cur.execute("alter table storm_business add past_5 int;" )
+#cur.execute("alter table storm_business drop past_1 ;")
+#cur.execute("alter table storm_business drop past_2 ;")
+#cur.execute("alter table storm_business drop past_3 ;")
+#cur.execute("alter table storm_business drop past_4 ;")
+#cur.execute("alter table storm_business drop past_5 ;")
+
+cur.execute("select column_name from information_schema.columns where table_name = 'storm_business';")
+print(cur.fetchall())
+#print(cur.fetchall())
 #cur.execute("alter table storm_student drop bar_number ;")
 cur.execute("select * from storm_student LIMIT 1;")
 print(cur.fetchall())
