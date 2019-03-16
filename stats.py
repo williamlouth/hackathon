484218b0fe09f35fdcbff7945ad89b63dac0b1ff
william <williamlouth@outlook.com>
2019-03-16 18:20:21 +0000

    0to1 not 0to5

--- stats.py
+++ stats.py
@@ -1,5 +1,5 @@
 import numpy as np
-dist = np.array([2440, 603, 128, 54, 17])
+#dist = np.array([2440, 603, 128, 54, 17])
 
 def normalize(distribution, grade):
     return np.sum(distribution[5 - grade:]) / np.sum(distribution)
